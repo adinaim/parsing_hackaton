40b8ab045eb3a79c86db4d4b5304e191f61b4346
adinaim <adinaitm@gmail.com>
2022-09-06 02:07:24 +0600

    уменьшено кол-во страниц с 895 до 15 для экономии времени

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-"""
-1. Получить html-код страницы
-2. Получить карточки из html-кода
-3. Распарсить данные с карточек
-4. Полученные данные записать в csv
-"""
-
-
-
-
 import csv
 import requests
 from bs4 import BeautifulSoup
@@ -34,7 +24,6 @@ def get_card_from_html(html: str) -> ResultSet:
     """ Функция для получения карточек из html-кода """
     soup = BeautifulSoup(html, 'lxml')
     cards: ResultSet = soup.find_all('div', class_='list-item list-label')
-    # print(cards)
     return cards
 
 
@@ -44,10 +33,14 @@ def parse_data_from_cards(cards: ResultSet) -> list:
     result = []
     for card in cards:
         try:
-            image_link = card.find('a').find('div', class_='thumb-item-carousel brazzers-daddy').find('div', class_='image-wrap').find('img', class_='lazy-image visible').get('src')
+            image_link = list(card.find('a').find('div', class_='thumb-item-carousel').find_all('img', class_='lazy-image'))[0].get('data-src')
+            # image_link = image.get('data-src')
         except AttributeError:
             image_link = 'Нет картинки'
-    
+
+        # единственное проблемное авто - <img class="lazy-image visible" alt="Фото авто Volkswagen Vento" title="" src="https://im.mashina.kg/tachka/images//1/7/f/17f704208220317ddebfce5e1691429c_240x180.jpg" style="display: block;">
+        except IndexError:
+            image_link = "https://im.mashina.kg/tachka/images//1/7/f/17f704208220317ddebfce5e1691429c_240x180.jpg"
         try:
             title = card.find('a').find('div', class_='block title').find('h2').text.strip()
         except AttributeError:
@@ -59,12 +52,12 @@ def parse_data_from_cards(cards: ResultSet) -> list:
             price = ''
         
         try:
-            description1 = card.find('a').find('div', class_='block info-wrapper item-info-wrapper').find('p',class_='year-miles').text.strip()
+            description1 = card.find('a').find('div', class_='block info-wrapper item-info-wrapper').find('p',class_='year-miles').text.strip() + ', '
         except AttributeError:
             description1 = ''
 
         try:
-            description2 = card.find('a').find('div', class_='block info-wrapper item-info-wrapper').find('p',class_='body-type').text.strip()
+            description2 = card.find('a').find('div', class_='block info-wrapper item-info-wrapper').find('p',class_='body-type').text.strip() + ', '
         except AttributeError:
             description2 = ''
 
@@ -81,6 +74,7 @@ def parse_data_from_cards(cards: ResultSet) -> list:
             'description': description,
             'image_link': image_link,
         }
+
         result.append(obj)
     return result
 
@@ -89,7 +83,7 @@ def parse_data_from_cards(cards: ResultSet) -> list:
 def write_to_csv(data: list):
     """ Запись данных в csv файл """
     fieldnames = ['title', 'price', 'description', 'image_link']
-    with open('cars.csv', 'w') as file:
+    with open('cars_small.csv', 'w') as file:
         csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
         csv_writer.writeheader()
         csv_writer.writerows(data)
@@ -97,18 +91,19 @@ def write_to_csv(data: list):
 
 
 def get_last_page(category):
+    """ Получение количества страниц """
     html = get_html(HOST, category)
     soup = BeautifulSoup(html, 'lxml')
     total_pages = soup.find('div', class_='search-results-table').find('nav').find('ul', class_='pagination').find_all('li', class_='page-item')[-1]
     last_page = total_pages.find('a').get('data-page')
-    print(int(last_page))
     return int(last_page)
 
 
 
 def main(category):
     result = []
-    for page in range(1, get_last_page(category)+1):
+    # for page in range(1, get_last_page(category)+1):
+    for page in range(1, get_last_page(category)-880):
         html = get_html(HOST, category, params=f'page={page}', headers=HEADERS)
         cards = get_card_from_html(html)
         list_of_cards = parse_data_from_cards(cards)
@@ -119,92 +114,3 @@ def main(category):
 
 if __name__ == '__main__':
     main('search')
-
-
-
-'''
-# Версия с урока
-
-
-def get_html(url):
-    headers = {"User-Agent":"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
-    html = requests.get(url, headers=headers, verify=False)
-    # print(response.status_code)
-    return html.text
-
-def get_total_pages(html):
-    soup = BeautifulSoup(html, 'lxml')
-    # print(soup)
-    print(soup.prettify())
-    pages = soup.find('div', class_='search-results-table').find('nav').find('ul', class_='pagination')
-    # print(pages)
-    total_pages = str(pages.find_all('li', class_='page-item'))[-1]
-    print(total_pages)
-    # last_page = total_pages.find('a', class_='page-link').get('href')
-    # last_page = total_pages.split('=')[-1]
-    # print(last_page)
-    # return int(last_page)
-
-def write_to_csv(data):
-    with open('mashina.csv', 'a') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow((data['title'],
-                         data['price'],
-                         data['description'],
-                         data['image']))
-
-def get_page_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    product_list = soup.find('div', class_='table-view-list image-view clr label-view')#.find('', class_='')
-    products = product_list.find_all('div', class_='list-item list-label').find('a')
-    # products1 = product_list.find_all('div', class_='list-item list-label ')
-    # products2 = product_list.find_all('div', class_='list-item list-label new-line')
-
-    for product in products:
-        try:
-            image = product.find('div', class_='image-wrap').find('img').get('src')
-        except:
-            image = ''
-
-        try:
-            title = product.find('div', class_='block title').find('h2').text.strip()
-        except:
-            title = ''
-
-        try:
-            price = product.find('div', class_='block price').find('p').find('br').text
-        except:
-            price = ''
-        
-        try:
-            description = product.find('div', class_='block info-wrapper item-info-wrapper').find_all('p').text
-        except:
-            description = ''
-
-        data = {'title': title, 'price': price, 'description': description, 'photo': image}
-        write_to_csv(data)
-
-
-def main(category):
-    auto_url = 'https://www.mashina.kg/'
-    auto1_url = 'https://www.mashina.kg/'
-
-    pages = '?page='
-   
-
-    # last_page = get_total_pages(get_html(auto_url))
-
-    # for page in range(1, last_page+1):
-    #     url_with_page = auto_url + category + '/all/' + pages + str(page)
-    #     html = get_html(url_with_page)
-    #     get_page_data(html)
-        
-
-# main('search')
-# main('commercialsearch')
-
-auto_url = 'https://www.mashina.kg/search/all/'
-last_page = get_total_pages(get_html(auto_url))
-for page in range(1, 2):#last_page+1):  
-    get_total_pages(get_html(auto_url + str(page)))
-'''
